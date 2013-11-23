23f8788de1f7eed711a93f4031c150525aabc68b
Arthur Macẽdo <arthur.jmacedo@gmail.com>
2013-11-22 21:07:21 -0300

    Demo files deleted

--- master/src/App.cpp
+++ master/src/App.cpp
@@ -2,24 +2,30 @@
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include "Gyroscope.h"
 #include <iostream>
 
 void *startSensor(void* arg){
-	Gyroscope gyro;
-	gyro.start();
+	//Gyroscope gyro;
+	//gyro.start();
+	while(true){
+		std::cout << "Sensor 1\n";
+		usleep(100000);
+	}
 }
 
 
 int main() {
 	
 	/*Turn on the thread sensor*/
-	pthread_t sensor;
+	pthread_t sensorThread;
 	int result;
-	result = pthread_create(&sensor, NULL, startSensor, NULL);
+	result = pthread_create(&sensorThread, NULL, startSensor, NULL);
 	if(result != 0 ){
 		std::cout << "Thread start got wrong";
 	}
+	pthread_join(sensorThread,NULL);
 	pthread_exit(NULL);
 	
 return 0;
