55191724f17deb607d4ca9b6208502c90ca2a73f
Firzen592798 <tyranitar_nb@hotmail.com>
2013-12-02 01:02:06 -0300

    rotacao melhorada

--- master/src/rotacao.cpp
+++ master/src/rotacao.cpp
@@ -16,17 +16,52 @@ void rotacaoVetor(float angulo, float ux, float uy, float uz, float r[][3]){
 	r[2][2] = cos(anguloRad) + (uz * uz) * (1 - cos(anguloRad));
 }
 
-int main(){
-	float angulo = 30;
-	float ux = 1;
-	float uy = 0;
-	float uz = 0;
-	float r[3][3];
-	rotacaoVetor(angulo, ux, uy, uz, r);
+
+
+void multiplicar(float matriz[][3], float vet[3]){
+	float result[3];
 	for(int i = 0; i < 3; i++){
-		for(int j = 0; j < 3; j++){
-			cout << r[i][j] <<"\t";
-		}
-		cout<<"\n";
+		result[i] = vet[0]* matriz[i][0] + vet[1]* matriz[i][1] + vet[2]* matriz[i][2];
+	}
+	vet[0] = result[0];
+	vet[1] = result[1];
+	vet[2] = result[2];
+}
+
+void rotacaoX(float vetor[3], float angulox){
+	float matrizRotacao[3][3];
+	rotacaoVetor(angulox, 1, 0, 0, matrizRotacao);
+	multiplicar(matrizRotacao, vetor);
+
+}
+
+void rotacaoY(float vetor[3], float anguloy){
+	float matrizRotacao[3][3];
+	rotacaoVetor(anguloy, 0, 1, 0, matrizRotacao);
+	multiplicar(matrizRotacao, vetor);
+
+}
+
+void rotacaoZ(float vetor[3], float anguloz){
+	float matrizRotacao[3][3];
+	rotacaoVetor(anguloz, 0, 0, 1, matrizRotacao);
+	multiplicar(matrizRotacao, vetor);
+
+}
+
+	
+	
+
+int main(){
+	float vetor[3];
+	vetor[0] = 1;
+	vetor[1] = 1;
+	vetor[2] = 1;
+	
+	rotacaoX(vetor, 30);
+	//rotacaoY(vetor, 30);
+	//rotacaoZ(vetor, 30);
+	for(int i = 0; i < 3; i++){
+			cout << vetor[i] <<"\t";
 	}
 }
